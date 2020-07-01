b9eede9f35690fcabeb5022d4042a9edec3accf9
gv4383 <gregory.vargas18@gmail.com>
2020-07-01 15:15:51 -0500

    Create downloadXML method

--- app/src/main/java/com/skwerlhub/top10downloader/MainActivity.kt
+++ app/src/main/java/com/skwerlhub/top10downloader/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import java.io.IOException
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -14,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG, "onCreate called")
         val downloadData = DownloadData()
-        downloadData.execute("URL goes here")
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
         Log.d(TAG, "onCreate: done")
     }
 
@@ -27,9 +32,67 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, "onPostExecute: parameter is $result")
             }
 
-            override fun doInBackground(vararg p0: String?): String {
-                Log.d(TAG, "doInBackground: starts with ${p0[0]}")
-                return "doInBackground completed"
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${url[0]}")
+                val rssFeed = downloadXML(url[0])
+
+                if (rssFeed.isEmpty()) {
+                    Log.e(TAG, "doInBackground: Error downloading")
+                }
+
+                return rssFeed
+            }
+
+            private fun downloadXML(urlPath: String?): String {
+                val xmlResult = StringBuilder()
+
+                try {
+                    val url = URL(urlPath)
+                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                    val response = connection.responseCode
+                    Log.d(TAG, "downloadXML: The response code was $response")
+
+//                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+//                    val inputBuffer = CharArray(500)
+//                    var charsRead = 0
+//
+//                    while (charsRead <= 0) {
+//                        charsRead = reader.read(inputBuffer)
+//
+//                        if (charsRead > 0) {
+//                            xmlResult.append(String(inputBuffer, 0, charsRead))
+//                        }
+//                    }
+//
+//                    reader.close()
+                    connection.inputStream.buffered().reader().use { xmlResult.append(it.readText()) }
+                    Log.d(TAG, "Received ${xmlResult.length} bytes")
+
+                    return xmlResult.toString()
+//                } catch (e: MalformedURLException) {
+//                    Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
+//                } catch (e: IOException) {
+//                    Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
+//                } catch (e: SecurityException) {
+//                    e.printStackTrace()
+//                    Log.e(TAG, "downloadXML: Security exception. Needs permissions? ${e.message}")
+//                } catch (e: Exception) {
+//                    Log.e(TAG, "downloadXML: Unkown error: ${e.message}")
+//                }
+                } catch (e: Exception) {
+                    val errorMessage: String = when (e) {
+                        is MalformedURLException -> "downloadXML: Invalid URL ${e.message}"
+                        is IOException -> "downloadXML: IO Exception reading data: ${e.message}"
+                        is SecurityException -> { e.printStackTrace()
+                            "downloadXML: Security Exception. Needs permission? ${e.message}"
+                        }
+                        else -> "Unkown error: ${e.message}"
+                    }
+                }
+
+                // If it gets to here, there's been a problem
+                // Return an empty string
+                return ""
             }
         }
     }
