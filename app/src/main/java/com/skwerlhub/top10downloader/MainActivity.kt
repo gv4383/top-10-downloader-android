fa13b0a79a488e44b91fe510bbe4bd3d08351854
gv4383 <gregory.vargas18@gmail.com>
2020-07-01 19:13:22 -0500

    Create FeedEntry class to parse xml data

--- app/src/main/java/com/skwerlhub/top10downloader/MainActivity.kt
+++ app/src/main/java/com/skwerlhub/top10downloader/MainActivity.kt
@@ -6,6 +6,23 @@ import android.os.Bundle
 import android.util.Log
 import java.net.URL
 
+class FeedEntry {
+    var name: String = ""
+    var artist: String = ""
+    var releaseDate: String = ""
+    var summary: String = ""
+    var imageURL: String = ""
+
+    override fun toString(): String {
+        return """
+            name = $name
+            artist = $artist
+            releaseDate = $releaseDate
+            imageURL = $imageURL
+        """.trimIndent()
+    }
+}
+
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
